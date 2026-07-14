b28cfc846045b8d19e446c13adf9a1d3b0c4a083
fjoknikstry-boop <fjoknikstry@gmail.com>
2026-07-14 08:05:01 +0700

    Update vallent.py

--- vallent.py
+++ vallent.py
@@ -56,7 +56,7 @@ import antinuke
 # ══════════════════════════════════════════════════════════════════
 
 BOT_NAME      = "VALLENT EXS"
-BOT_TAGLINE   = "No mercy. No limits. Full control."
+BOT_TAGLINE   = "Nocturne Development."
 BOT_VERSION   = "1.0.0"
 BOT_PREFIX    = "!vx "
 CONFIG_PATH   = "data/config.json"
